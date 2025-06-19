38408e4157aaedbdb4740272ebc3931cbb9d497b
Icestonks <Icestonks@gmail.com>
2025-06-19 16:47:26 +0200

    cache getQueryClient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { trpc } from "@/lib/trpc/server";
+import { getQueryClient, trpc } from "@/lib/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import PageClient from "./page-client";
 import Link from "next/link";
-import { createQueryClient } from "@/lib/trpc/query-client";
 
 export default async function Page() {
     // prefetch(trpc.users.getAmountOfUsers.queryOptions());
-    const queryClient = createQueryClient()
+    const queryClient = getQueryClient();
 
-    void queryClient.prefetchQuery(trpc.users.getAmountOfUsers.queryOptions({}));
+    void queryClient.prefetchQuery(
+        trpc.users.getAmountOfUsers.queryOptions({})
+    );
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
